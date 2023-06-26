b3ff4bef21343b9c6e5d25da0aab215c22c413c1
Victor Volpi <112173184+VictorVolpi@users.noreply.github.com>
2023-06-26 16:14:59 -0300

    ;

---
+++ Atividade - 5/exercicios.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+   
+    $cliente = array(
+        'comer' =>  "<img src = 'images/comer.jpg '>" 
+        ,'futebol' =>  "<img src = 'images/ney.jpg '>" 
+        ,'correr img'=> "<img src = 'images/Motivacao-para-correr.jpg '>"
+        ,'jogar video game' =>  "<img src = 'images/video game.jpg'>"
+        ,'dormir' =>  "<img src = 'images/dormir.jpg '>"
+    );
+
+    print_r($cliente);
+?>
+
+</body>
+</html>
+
+
+
