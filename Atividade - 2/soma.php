d2c102d783e53ed335392edc05466051fc538b28
Victor Volpi <112173184+VictorVolpi@users.noreply.github.com>
2023-06-26 16:11:02 -0300

    .

---
+++ Atividade - 2/soma.php
@@ -0,0 +1,12 @@
+
+
+    <?php
+
+        $numero1 = $_POST['caixa1'];
+        $numero2 = $_POST['caixa2'];
+
+        $soma =$numero1 + $numero2;
+
+        echo "O resultado da soma é: " .$soma;
+    ?>    
+
